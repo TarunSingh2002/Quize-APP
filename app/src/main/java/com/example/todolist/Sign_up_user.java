edc10f1397704a2822343cff96144fafc2eaf87b
Tarun Singh <tarun2002.ts02@gmail.com>
2023-11-18 21:55:09 +0530

    Fifth_commit

--- app/src/main/java/com/example/todolist/Sign_up_user.java
+++ app/src/main/java/com/example/todolist/Sign_up_user.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.animation.Animator;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
 
@@ -13,6 +14,7 @@ import com.airbnb.lottie.LottieAnimationView;
 
 public class Sign_up_user extends AppCompatActivity {
 boolean isChecked=false;
+private ALoadingDialog aLoadingDialog;
 LottieAnimationView checkBoxAnimation;
 TextView login_page , login_page_two;
     @Override
@@ -22,6 +24,7 @@ TextView login_page , login_page_two;
         checkBoxAnimation = findViewById(R.id.anima);
         login_page=findViewById(R.id.login_page_transfer);
         login_page_two=findViewById(R.id.login_page_transfer_two);
+        aLoadingDialog = new ALoadingDialog(this);
         login_page.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,9 +36,17 @@ TextView login_page , login_page_two;
         login_page_two.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Sign_up_user.this, login.class);
+                /*Intent intent = new Intent(Sign_up_user.this, login.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-                startActivity(intent);
+                startActivity(intent);*/
+                aLoadingDialog.show();
+                Handler handler = new Handler();
+                Runnable runnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        aLoadingDialog.cancel();
+                    }
+                };handler.postDelayed(runnable,7000);
             }
         });
         checkBoxAnimation.setOnClickListener(new View.OnClickListener() {
@@ -53,6 +64,7 @@ TextView login_page , login_page_two;
                 }
             }
         });
+
 
     }
 }
