8cb4fbd917bd9c5f4c69a1dd9d3a61a74fefce92
Tarun Singh <tarun2002.ts02@gmail.com>
2023-12-26 02:15:10 +0530

    27th_commit

--- app/src/main/java/com/example/todolist/MainActivity2.java
+++ app/src/main/java/com/example/todolist/MainActivity2.java
@@ -6,15 +6,23 @@ import androidx.appcompat.widget.AppCompatButton;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    private ImageView imageViewHome;
+    private TextView textViewHome;
     AppCompatButton profile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        profile=findViewById(R.id.profile);
+        imageViewHome=findViewById(R.id.imageViewHome);
+        textViewHome=findViewById(R.id.textViewHome);
+        textViewHome.setTextColor(getResources().getColor(R.color.golden));
+        imageViewHome.setImageResource(R.drawable.home_focused);
+        /*profile=findViewById(R.id.profile);
         profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -22,6 +30,6 @@ public class MainActivity2 extends AppCompatActivity {
                 intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
                 startActivity(intent);
             }
-        });
+        });*/
     }
 }
